c3611fd9e2ca5501cc03d4eff65ba1c3f57b36a9
unknown <desarrolloweb@disdelsa.com>
2020-10-05 23:32:49 -0600

    nuevos cambios

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const express = require("express");
+const http=require("http");
 const app = express();
-const serialPort = require("serialport");
-const Readline = serialPort.parsers.Readline;
-
 const PORT = process.env.PORT || 4000;
+const servidor=http.createServer(app);
+const socketio=require("socket.io")
+const io=socketio(servidor);
+///arduino
+const serialPort = require("serialport");
+let Readline = serialPort.parsers.Readline;
+let Board = new serialPort("COM3", {
+    baudRate: 9600,
+  });
+let parser = Board.pipe(new Readline({ delimiter: "\r\n" }));
+  
+io.on('connection',socket=>{
+    socket.on('conectado',()=>{
 
-const Board = new serialPort("COM3", {
-  baudRate: 9600,
-});
+        console.log("arduino conectado")
+      
+          parser.on("open", function () {
+            console.log("Conexion Abierta");
+          });
+          
+          parser.on("data", function (data) {
+            console.log(data);
+          });
+          
+          Board.on("error", function (error) {
+            console.log(error);
+          });
+          console.log("jdjdj");
+          
+    })
+})
 
-const parser = Board.pipe(new Readline({ delimiter: "\r\n" }));
 
-parser.on("open", function () {
-  console.log("Conexion Abierta");
-});
-
-parser.on("data", function (data) {
-  console.log(data);
-});
-
-Board.on("error", function (error) {
-  console.log(error);
-});
-console.log("jdjdj");
-app.get("/", (req, res) => {
-  res.send("Desde mi codigo de arduino");
-});
-
-app.listen(PORT, () => {
+///
+servidor.listen(PORT, () => {
   console.log(`el puerto esta funcionando en el puerto ${PORT}`);
 });
