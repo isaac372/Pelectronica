1c20042b44c6d878940f0513d4ced6e8504c4a3c
unknown <desarrolloweb@disdelsa.com>
2020-10-15 03:29:13 -0600

    cambios servidor

--- index.js
+++ index.js
@@ -4,34 +4,41 @@ const app = express();
 const servidor = http.createServer(app);
 const socketio = require("socket.io");
 const io = socketio(servidor);
-//arduino
-const serialPort = require("serialport");
-let Readline = serialPort.parsers.Readline;
-let Board = new serialPort("COM3", {
+const SerialPort = require("serialport");
+
+
+const Board = new SerialPort("COM3", {
   baudRate: 9600,
 });
-let parser = Board.pipe(new Readline({ delimiter: "\r\n" }));
+
+Board.on('open', function () {
+  console.log("Conexion Abierta");
+});
+
+Board.on('data', function(data){
+  let temp =parseInt(data,10)+" ºC";
+  console.log(temp);
+  io.emit("temperatura", temp);
+})
+
+
+Board.on("error", function (error) {
+  console.log(error);
+});
 
 io.on("connection", (socket) => {
   socket.on("conectado", () => {
-console.log("conectado")
+console.log("conectado desde cliente")
 
-    parser.on("open", function () {
-      console.log("Conexion Abierta");
-    });
-    parser.on("data", function (data) {
-     // io.emit("temperatura", data);
-    });
-    io.emit("temperatura", "casa");
-
-    Board.on("error", function (error) {
-      console.log(error);
-    });
+   
   });
 });
 
-///
+
+
 const PORT = process.env.PORT || 4000;
 servidor.listen(PORT, () => {
   console.log(`el puerto esta funcionando  ${PORT}`);
 });
+
+
