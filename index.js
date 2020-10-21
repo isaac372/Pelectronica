f98b3e3406149747eb20b9c86290871c0ecbbe71
unknown <desarrolloweb@disdelsa.com>
2020-10-21 00:00:45 -0600

    cambios

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const app = express();
+app.use(cors());
 const servidor = http.createServer(app);
 const socketio = require("socket.io");
 const io = socketio(servidor);
