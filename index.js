f37e3085120a2c62a22633af7b235ac95c8f9ca7
unknown <desarrolloweb@disdelsa.com>
2020-10-23 01:59:02 -0600

    cambios

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const http = require("http");
-const servidor = http.createServer(app.use(cors(),bodyParser.json()));
+const servidor = http.createServer(app.use(cors(), bodyParser.json()));
 const socketio = require("socket.io");
-
 const io = socketio(servidor);
 const { Board, Sensor, Led } = require("johnny-five");
 const board = new Board();
@@ -15,25 +14,55 @@ const modelTemp = require("./models/Modeltemp");
 const conectarDB = require("./config/db");
 conectarDB();
 
-let ldr, lm35, celsuistemp;
-let led11, led12, led13;
+//Sensores
+let MQ135, LM35, LDR;
+//Led a Encender
+let celsuistemp, ledAire, ledC02, ledGas, led11, led12, led13;
+let valueM0="",valueM1="",valueM2="";
 
 board.on("ready", () => {
-  const SendorTemperatura = { pin: "A0", freq: 50 };
-  const SendorLuz = { pin: "A1", freq: 50 };
-  lm35 = new Sensor(SendorTemperatura);
-  ldr = new Sensor(SendorLuz);
+  const Sensor_Aire_CO2_Gas = { pin: "A0", freq: 50 };
+  const SendorTemperatura = { pin: "A1", freq: 50 };
+  const SendorLuz = { pin: "A2", freq: 50 };
+
+  MQ135 = new Sensor(Sensor_Aire_CO2_Gas);
+  LM35 = new Sensor(SendorTemperatura);
+  LDR = new Sensor(SendorLuz);
 
   ondear();
 });
 
 const ondear = async () => {
-  const temp = lm35.value;
-  const luz = ldr.value;
+  const valorMq135 = MQ135.value;
+  const valorTemperatura = LM35.value;
+  const valorLuz = LDR.value;
+  ledAire = new Led(8);
+  ledC02 = new Led(9);
+  ledGas = new Led(10);
   led11 = new Led(11);
   led12 = new Led(12);
   led13 = new Led(13);
-  celsuistemp = (5 * temp * 100) / 1024;
+ console.log(valorMq135)
+  //MQ135//
+  if (valorMq135 <= 55) {
+    ledAire.on();
+    ledC02.off();
+    ledGas.off();
+    valueM0=valorMq135;
+  }  if (valorMq135 >= 56 && valorMq135 <= 65) {
+    ledAire.off();
+    ledC02.on();
+    ledGas.off();
+    valueM1=valorMq135;
+  }  if (valorMq135 >= 74 && valorMq135 <= 350) {
+    ledAire.off();
+    ledC02.off();
+    ledGas.on();
+    valueM2=valorMq135;
+  }
+
+  //Temperatura//
+  celsuistemp = (5 * valorTemperatura * 100) / 1024;
   if (celsuistemp > 22) {
     led13.on();
     led12.off();
@@ -42,7 +71,8 @@ const ondear = async () => {
     led13.off();
   }
 
-  if (luz < 75) {
+  //Luz//
+  if (valorLuz < 75) {
     led11.on();
   } else {
     led11.off();
@@ -50,22 +80,25 @@ const ondear = async () => {
   try {
     let body = {
       temperatura: parseInt(celsuistemp),
-      luminicidad: luz,
+      luminicidad: valorLuz,
+      aire:valueM0,
+      co2:valueM1,
+      gas:valueM2
     };
 
-    // console.log(body)
+    //console.log(body)
     // User=new modelTemp(body);
     //   await User.save()
 
     const ver = await modelTemp.find({}).sort({ _id: -1 }).limit(1);
 
-    console.log(ver);
+     console.log(ver);
     io.emit("temperatura", ver);
   } catch (error) {
     console.log(error);
   }
 
-  setTimeout(ondear, 5000);
+  setTimeout(ondear, 3000);
 };
 
 const PORT = process.env.PORT || 4000;
--- models/Modeltemp.js
+++ models/Modeltemp.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
+  aire: {
+    type: Number,
+  },
+  co2: {
+    type: Number,
+  },
+  gas: {
+    type: Number,
+  },
   temperatura: {
     type: Number,
   },
-  luminicidad:{
-    type:Number
-  }
+  luminicidad: {
+    type: Number,
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
